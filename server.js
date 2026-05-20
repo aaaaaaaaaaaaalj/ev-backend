5ddab17efcb3177ef01d858b9a935c1e81be17b0
aaaaaaaaaaaaalj <lucaslfmax@gmail.com>
2026-05-20 10:39:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/login", (req, res) => {
-  const url = `https://oauth.sandbox.enode.io/oauth2/token`;
+  const url = `https://api.enode.io/oauth/authorize?client_id=a7be4e42-9e37-47c0-a935-66124b47ace6&response_type=code&redirect_uri=https://ev-backend-259f.onrender.com/callback`;
 
   res.redirect(url);
 });
-
 
 // 🔥 IMPORTANT RENDER
 const PORT = process.env.PORT || 10000;
